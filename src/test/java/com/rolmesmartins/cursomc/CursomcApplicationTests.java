dc66aee1785cf187a669ef690693a697abf451cf
Rolmes Martins <rolmes20@gmail.com>
2021-07-15 13:31:20 -0300

    Projeto criado

---
+++ src/test/java/com/rolmesmartins/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolmesmartins.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
